29ec51612454dc8ad2fc4329bbb02ed61da4bf98
vinicius souza de jesus <vinisouzadejesus26@gmail.com>
2025-10-15 21:06:08 -0300

    new commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
 
 const Header = () => {
     return (
@@ -12,7 +13,8 @@ const Header = () => {
              <nav className="hidden sm:block">
                  <NavItems />
              </nav>
-             {/*    UserDropdown*/}
+
+             <UserDropdown />
          </div>
      </header>
     )
